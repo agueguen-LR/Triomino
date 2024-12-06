9deb588e9778575a59f152b0145eb5c5a72b2d80
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-12-06 20:55:06 +0100

    Outputs to file

--- src/triomino-generate.c
+++ src/triomino-generate.c
@@ -1,24 +1,46 @@
 /**
-* @file       triomino-generate.c
+ * @file       triomino-generate.c
  *
  * @author     Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
  * @date       2024
  * @copyright
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "triomino.h"
 
-int main() {
+#include "triomino.inc"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Please add name of target file"); // NOLINT build/include_what_you_use
+    return 1;
+  }
+
+  FILE *file = fopen(argv[1], "wb");
+  if (!file) {
+    perror("Error opening file");
+    return EXIT_FAILURE;
+  }
+
+  triomino_init();
+
   for (int a = 0; a <= 5; a++) {
     for (int b = a; b <= 5; b++) {
       for (int c = b; c <= 5; c++) {
         Triomino* triomino = triomino_create_full(a, b, c);
         if (triomino) {
-          printf("%s\n", triomino_to_string(triomino)); // NOLINT build/include_what_you_use
+          fwrite(triomino, sizeof(Triomino), 1, file); // NOLINT build/include_what_you_use
           triomino_free(triomino);
         }
       }
     }
   }
-  return 0;
+
+  triomino_finish();
+  fclose(file); // NOLINT build/include_what_you_use
+
+  return EXIT_SUCCESS;
 }
