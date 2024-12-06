0e70a488ca5d67edff9f3193e918090b5535e939
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-12-06 22:22:56 +0100

    Verifies count and unique

--- test/test-generate.c
+++ test/test-generate.c
@@ -9,6 +9,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "triomino.h"
 #include "triomino.inc"
@@ -19,7 +20,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  FILE *file = fopen(argv[1], "wb");
+  FILE *file = fopen(argv[1], "rb");
   if (!file) {
     perror("Error opening file");
     return EXIT_FAILURE;
@@ -29,14 +30,32 @@ int main(int argc, char *argv[]) {
 
   // Verify: Check the contents of the file
   assert(file != NULL);
-  Triomino* triomino;
+
+  int triomino_count = 0;
+  int triomino_count_expected = 56;
+  Triomino* triomino = triomino_create_default();
+  Triomino* unique_triominos[56];
+
   while (fread(triomino, sizeof(Triomino), 1, file) == 1) {
       // Add assertions to verify the contents of each triomino
       assert(triomino->a >= 0 && triomino->a <= 5);
       assert(triomino->b >= triomino->a && triomino->b <= 5);
       assert(triomino->c >= triomino->b && triomino->c <= 5);
+
+      // Ensure each triomino is unique
+      for (int i = 0; i < triomino_count; i++) {
+        assert(triomino->a != unique_triominos[i]->a || triomino->b != unique_triominos[i]->b || triomino->c != unique_triominos[i]->c);
+      }
+      unique_triominos[triomino_count] = triomino_clone(triomino);
+
+      triomino_count++;
   }
   fclose(file); // NOLINT build/include_what_you_use
+  assert(triomino_count == triomino_count_expected);
+  triomino_destroy(triomino);
+  for (int i = 0; i < triomino_count; i++) {
+    triomino_destroy(unique_triominos[i]);
+  }
 
   triomino_finish();
 }
